b74e24707c32c33ba6bffe7975c07a457e215f10
Anastasia <maksimovskaya@cleverstaff.net>
2023-10-05 23:27:13 +0300

    add pagination to orders

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,17 +1,52 @@
 import './Orders.css'
-import axios from "axios";
 import {useEffect, useState} from "react";
 import axiosInstance from "./AxiosInstance";
+import Pagination from "./Pagination";
+import {useSearchParams} from "react-router-dom";
 
 
-export default function Orders (props) {
+export default function Orders(props) {
 
     const [items, setItems] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [total, setTotal] = useState();
 
-    const response = () => axiosInstance
-        .get(`http://localhost:8080/order/getOrders`).then(r => {
+
+    const pg = searchParams.get("pg") ?? 1;
+
+    const pageSize = searchParams.get("size") ?? 5;
+
+    const searchData = {
+        "page": pg,
+        "pageSize": pageSize
+    };
+
+    useEffect(() => {
+        axiosInstance
+            .post(`http://localhost:8080/order/getOrders`, searchData).then(r => {
             if (r !== undefined && r.data !== undefined) {
                 setItems(r.data.objects)
+                setTotal(r.data.total)
+            }
+        });
+    }, [pg, pageSize])
+
+
+    function changePerPage(e) {
+        let currentPg;
+        if (total <= e.target.value * (pg - 1)) {
+            currentPg = 1;
+        } else {
+            currentPg = searchParams.get("pg") ?? 1;
+        }
+        setSearchParams({'size': e.target.value, 'pg': currentPg});
+    }
+
+    const response = () => axiosInstance
+        .post(`http://localhost:8080/order/getOrders`, searchData).then(r => {
+            if (r !== undefined && r.data !== undefined) {
+                setItems(r.data.objects)
+                setTotal(r.data.total)
             }
         });
 
@@ -34,6 +69,22 @@ export default function Orders (props) {
                         </div>
                     </>
                 ))}
+                <div className="pagination-content">
+                    <Pagination totalCount={total}/>
+                    <div className="per-page">
+                        <span>Показать на странице</span>
+                        <select
+                            onChange={e => {
+                                changePerPage(e)
+                            }}
+                            name="select">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </>
     )
---
+++ src/components/Pagination.js
@@ -0,0 +1,107 @@
+import './Pagination.css'
+import {useSearchParams} from "react-router-dom";
+
+export default function Pagination(props) {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const pg = searchParams.get("pg") ?? 1;
+
+    const pageSize = searchParams.get("size") ?? 5;
+
+
+    const {
+        totalCount,
+        siblingCount = 1,
+    } = props;
+
+
+    let pageCount;
+    if (totalCount % pageSize === 0) {
+        pageCount = totalCount / pageSize;
+    } else {
+        pageCount = Math.floor(totalCount / pageSize) + 1;
+    }
+
+
+    function refreshSearch(e) {
+        setSearchParams({'pg': e.target.value, 'size': pageSize});
+    }
+
+    function createCssStyle(pageNumber) {
+        const className = ['visible-page'];
+        if (pageNumber == pg) {
+            className.push('current-page');
+        }
+        return className.join(', ');
+    }
+
+    return (
+        <div className="pagination">
+            {(() => {
+                const arr = [];
+
+                if (pg > Number(siblingCount) + 1) {
+                    arr.push(
+                        <button className={createCssStyle(1)} onClick={refreshSearch} value={1}>
+                            {1}
+                        </button>
+                    );
+                    if (pg > Number(siblingCount) + 2) {
+                        const hiddenPages = [];
+                        for (let j = 2; j < pg - siblingCount; j++) {
+                            hiddenPages.push(<option value={j}>{j}</option>)
+                        }
+                        arr.push(<div className="hidden-pages">
+                            <button className="dropbtn">...</button>
+                            <select
+                                className="dropdown-content"
+                                onChange={e => {
+                                    refreshSearch(e)
+                                }}
+                                name="select">
+                                {hiddenPages}
+                            </select>
+                        </div>)
+                    }
+                }
+
+                for (let i = Number(pg) - Number(siblingCount) > 0 ? Number(pg) - Number(siblingCount) : 1; i <= ((Number(pg) + Number(siblingCount)) > pageCount ? pageCount : Number(pg) + Number(siblingCount)); i++) {
+                    arr.push(
+                        <button className={createCssStyle(i)} onClick={refreshSearch} value={i}>
+                            {i}
+                        </button>
+                    );
+                }
+
+                if (pg < pageCount - Number(siblingCount) - 1) {
+                    const hiddenPages = [];
+                    for (let j = Number(pg) + Number(siblingCount) + 1; j < pageCount; j++) {
+                        hiddenPages.push(<option value={j}>{j}</option>)
+                    }
+                    arr.push(<div className="hidden-pages">
+                        <button className="dropbtn">...</button>
+                        <select
+                            className="dropdown-content"
+                            onChange={e => {
+                                refreshSearch(e)
+                            }}
+                            name="select">
+                            {hiddenPages}
+                        </select>
+                    </div>)
+                }
+
+                if (pg < Number(pageCount) - Number(siblingCount)) {
+                    arr.push(
+                        <button className={createCssStyle(pageCount)} onClick={refreshSearch} value={pageCount}>
+                            {pageCount}
+                        </button>
+                    );
+                }
+
+                return arr;
+            })()}
+        </div>
+    )
+}
